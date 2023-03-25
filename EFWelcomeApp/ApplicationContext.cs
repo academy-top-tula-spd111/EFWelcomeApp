c76721f6e472ada29a0c5b2b490a9006c0304c5c
maxefimov <efimov.academy.top@gmail.com>
2023-03-25 17:47:47 +0300

    Добавьте файлы проекта.

---
+++ EFWelcomeApp/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFWelcomeApp
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public ApplicationContext() 
+        {
+            //this.Database.EnsureDeleted();
+            //this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-ISC66B9\MSSQLSERVER2022;Database=CompanyDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
---
+++ EFWelcomeApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using EFWelcomeApp;
+
+/*
+using (ApplicationContext context = new ApplicationContext())
+{
+    Employee tom = new() { Name = "Tom", Age = 32 };
+    Employee bob = new() { Name = "Bob", Age = 27 };
+
+    context.Employees.Add(tom);
+    context.Employees.Add(bob);
+
+    context.SaveChanges();
+}
+*/
+
+using(ApplicationContext context = new ApplicationContext())
+{
+    var employes = context.Employees.ToList();
+    Console.WriteLine("Employees:");
+    foreach(Employee e in employes)
+        Console.WriteLine($"{e.Id}: {e.Name} ({e.Age})");
+}
